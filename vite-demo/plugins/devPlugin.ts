20d5032ba9540d0c80d0bc831d03acd3e243db90
cell <cellinlab@gmail.com>
2023-08-30 23:02:01 +0800

    build

--- vite-demo/plugins/devPlugin.ts
+++ vite-demo/plugins/devPlugin.ts
@@ -1,5 +1,8 @@
 import { ViteDevServer } from 'vite'
 
+import path from 'path';
+import fs from 'fs';
+
 export const devPlugin = () => {
   return {
     name: 'dev-plugin',
@@ -36,7 +39,7 @@ export const devPlugin = () => {
 
 export const getReplacer = () => {
   const externalModels = ["os", "fs", "path", "events", "child_process", "crypto", "http", "buffer", "url", "better-sqlite3", "knex"];
-  const result = {};
+  const result: { [key: string]: any } = {}; // add index signature
   for (let item of externalModels) {
     result[item] = () => ({
       find: new RegExp(`^${item}$`),
@@ -51,4 +54,76 @@ export const getReplacer = () => {
     };
   };
   return result;
+}
+
+class BuildObj {
+  buildMain() {
+    require("esbuild").buildSync({
+      entryPoints: ["./src/main/mainEntry.ts"],
+      bundle: true,
+      platform: "node",
+      outfile: "./dist/mainEntry.js",
+      external: ["electron"],
+    });
+  }
+
+  preparePackageJson() {
+    const packJsonPath = path.join(process.cwd(), "package.json");
+    const localPkgJson = JSON.parse(fs.readFileSync(packJsonPath, "utf-8"));
+    const electronConfig = localPkgJson.devDependencies.electron.replace("^", "");
+    localPkgJson.main = "mainEntry.js";
+    delete localPkgJson.scripts;
+    delete localPkgJson.devDependencies;
+    localPkgJson.devDependencies = {
+      electron: electronConfig,
+    };
+    const tarJsonPath = path.join(process.cwd(), "dist", "package.json");
+    fs.writeFileSync(tarJsonPath, JSON.stringify(localPkgJson, null, 2));
+    fs.mkdirSync(path.join(process.cwd(), "dist/node_modules"));
+  }
+
+  buildInstaller() {
+    const options = {
+      config: {
+        directories: {
+          output: path.join(process.cwd(), "release"),
+          app: path.join(process.cwd(), "dist"),
+        },
+        files: ["**"],
+        extends: null,
+        productName: "ViteElectronDemo",
+        appId: "com.vite-electron.demo",
+        asar: true,
+        nsis: {
+          oneClick: true,
+          perMachine: true,
+          allowToChangeInstallationDirectory: false,
+          createDesktopShortcut: true,
+          createStartMenuShortcut: true,
+          shortcutName: "ViteElectronDemo",
+        },
+        publish: [
+          {
+            provider: "generic",
+            url: "http://localhost:5500",
+          },
+        ]
+      },
+      project: process.cwd(),
+    };
+
+    return require("electron-builder").build(options);
+  }
+}
+
+export const buildPlugin = () => {
+  return {
+    name: 'build-plugin',
+    closeBundle: () => {
+      const buildObj = new BuildObj();
+      buildObj.buildMain();
+      buildObj.preparePackageJson();
+      buildObj.buildInstaller();
+    }
+  };
 }
--- vite-demo/vite.config.ts
+++ vite-demo/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import optimizer from 'vite-plugin-optimizer'
 
-import { devPlugin, getReplacer } from './plugins/devPlugin'
+import { devPlugin, getReplacer, buildPlugin } from './plugins/devPlugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +11,11 @@ export default defineConfig({
     devPlugin(),
     react()
   ],
+  build: {
+    rollupOptions: {
+      plugins: [
+        buildPlugin()
+      ]
+    }
+  }
 })
